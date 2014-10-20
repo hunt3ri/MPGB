a47681c4a7e6e2e6fbcfb43697ceb8caa6efecd9
Iain Hunter <iain.hunter@outlook.com>
2014-10-20 23:20:38 +0100

    Initial mixpanel event

--- app/web/forms.py
+++ app/web/forms.py
@@ -1,11 +1,14 @@
 from flask.ext.wtf import Form, html5
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
 class FlightsForm(Form):
     name = StringField('Name?', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
     tel = html5.TelField('Mobile')
+    payment_type = SelectField(u'Payment Type', choices=[('amex', 'American Express'), ('mastercard', 'Master Card'), ('visa', 'Visa')])
+    additional_baggage = SelectField('Additional Bags', choices=[('0', '0'), ('1', '1'), ('2', '2')])
     submit = SubmitField('Submit')
 
 
