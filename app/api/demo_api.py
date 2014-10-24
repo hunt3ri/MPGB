31d475e79487b913b9858285809b0ac430b3f7fb
Iain Hunter <iain.hunter@outlook.com>
2014-10-24 08:50:51 +0100

    Map api test

--- app/api/demo_api.py
+++ app/api/demo_api.py
@@ -21,4 +21,21 @@ def get_latency():
     latency= dict(item=325, min=min, max=max)
     return jsonify(latency)
 
+@api.route('/api/get_locations', methods=['GET'])
+def get_locations():
+    london = dict(city_name="London", country_code="UK")
+    glasgow = dict(city_name="Glasgow", country_code="UK")
+    sydney = dict(city_name="Sydney", country_code="AU")
+    new_york = dict(city_name="New York", country_code="US")
+    cape_town = dict(city_name="Cape Town", country_code="ZA")
+
+    point1 = dict(london, size=10)
+    point2 = dict(sydney, size=5)
+
+    point_list = [point1, point2]
+    points = dict(point=point_list)
+
+
+    return jsonify(points=points)
+
 
