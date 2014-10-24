3b6540f7ff1984792b52fde8b81429b5592b08ae
Iain Hunter <iain.hunter@outlook.com>
2014-10-24 08:26:47 +0100

    Revenue Api

--- app/api/demo_api.py
+++ app/api/demo_api.py
@@ -8,4 +8,10 @@ def get_uptime():
     uptime = dict(status="Up", downTime="9 days", responseTime="500 ms")
     return jsonify(uptime)
 
+@api.route('/api/revenue', methods=['GET'])
+def get_revenue():
+    revenue = dict(text="Revenue Last 24 hours", value=4532, prefix="£")
+    item = [revenue]
+    return jsonify(item=item)
+
 
