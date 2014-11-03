ae29cf7d91205ee365b751896bc615129e217de3
Iain Hunter <iain.hunter@outlook.com>
2014-11-03 22:26:26 +0000

    grunt tweaks

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by iainhunter on 29/10/2014.
+ */
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+    })
+
+    grunt.registerTask("default", function () {
+        console.log("Hello World")
+    })
+}
